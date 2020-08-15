ae85b0d220929876393f06dec6f90e0ff1a8ff8b
PAULO GALDINO <jose.galdino4@fatec.sp.gov.br>
2020-08-15 18:49:41 -0300

    Denifido as classes dos enums do sistema na camada do Entity

---
+++ src/Entity/Enums/EnumTIpoPessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace Entity.Enums
+{
+    public enum EnumTIpoPessoa
+    {
+        Administrador = 1,
+        Comum = 2
+    }
+}
---
+++ src/Entity/Enums/EnumTipoDespesa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity.Enums
+{
+    public enum EnumTipoDespesa
+    {
+        Contas = 1,
+        Investimento = 2
+    }
+}
